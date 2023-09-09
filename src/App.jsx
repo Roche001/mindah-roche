bc1722cb337ef117ed493675345221bb3f92d051
Roche Harold <rocheharold13@gmail.com>
2023-09-09 13:13:22 +0300

    upddate changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 import "./index.css";
+import Top from "./components/top/Top";
+import Home from "./components/home/Home";
+import About from "./components/about/About";
+import Services from "./components/services/Services";
+import Portfolio from "./components/portfolio/Portfolio";
+import Contact from "./components/contact/Contact";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Top />
+      <Home />
+      <About />
+      <Services />
+      <Portfolio />
+      <Contact />
+    </>
+  );
 };
 
 export default App;
---
+++ src/nav.js
@@ -0,0 +1,6 @@
+export const bar = [
+  { title: "home", titleLink: "/" },
+  { title: "about", titleLink: "/About" },
+  { title: "services", titleLink: "/Services" },
+  { title: "portfolio", titleLink: "/Portfolio" },
+];
