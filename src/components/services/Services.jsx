4285edd430ef2c28c9b9e49259cb6b36a840d956
Roche Harold <rocheharold13@gmail.com>
2023-09-11 22:05:38 +0300

    update changes

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 import "./Services.css";
+import { services, info } from "../../nav";
 
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <div id="Services" className="services-container">
+      <div className="services-text">
+        <h5>Service Spectrum</h5>
+        {services.map((item, index) => {
+          return <p>{item.text}</p>;
+        })}
+      </div>
+      <div className="services-info">
+        {info.map((item, index) => {
+          return (
+            <div className="img">
+              <img src={item.image} alt="services" loading="lazy" />
+              <p>{item.title}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Services;
--- src/nav.js
+++ src/nav.js
@@ -18,3 +18,33 @@ export const text = [
       "I have over 3 years of experience working as data analyst/scientist majorly in the marketing advertising industry, which has given me opportunity to translate data into actionable insights.",
   },
 ];
+export const services = [
+  {
+    text: "I transform data into insightful and useful knowledge as a free-lance data scientist and analyst. My objectives are to enhance outcomes, make wise choices, and reduce expenses.",
+  },
+  {
+    text: "I have experience with supervised and unsupervised querying databases using SQL, performing data processing and visualization using excel, Power BI, Looker, and Microsoft Clarity, Ecommerce and Web Analytics using Google Analytics 4, and performing predicting modelling. For my data projects, I employ SQL, Python, QlikView,Looker and PowerVI.",
+  },
+];
+export const info = [
+  {
+    title:
+      "I build interactive dashboards to provide comprehensive representation of relevant data in key areas.",
+    image: "../../assets/sev1.png",
+  },
+  {
+    title:
+      "I specialize in analysing data to identify results, trends, and make recommendations base on finds.",
+    image: "../../assets/sev2.png",
+  },
+  {
+    title:
+      "Representing data using graphs is more appealing compared to texts. I create interactive infographics to make data visibility more appealing.",
+    image: "../../assets/sev3.png",
+  },
+  {
+    title:
+      "I use Machine Learning models to perform predictive analysis, which enables for making future performance predictions.",
+    image: "../../assets/sev4.png",
+  },
+];
