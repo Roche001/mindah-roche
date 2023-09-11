7a6abcff00abbad6ae55db3f6f8fefd1c10cd384
Roche Harold <rocheharold13@gmail.com>
2023-09-11 22:14:47 +0300

    update changes

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -5,21 +5,23 @@ import { services, info } from "../../nav";
 const Services = () => {
   return (
     <div id="Services" className="services-container">
-      <div className="services-text">
-        <h5>Service Spectrum</h5>
-        {services.map((item, index) => {
-          return <p>{item.text}</p>;
-        })}
-      </div>
-      <div className="services-info">
-        {info.map((item, index) => {
-          return (
-            <div className="img">
-              <img src={item.image} alt="services" loading="lazy" />
-              <p>{item.title}</p>
-            </div>
-          );
-        })}
+      <div className="services-about">
+        <div className="services-text">
+          <h5>Service Spectrum</h5>
+          {services.map((item, index) => {
+            return <p>{item.text}</p>;
+          })}
+        </div>
+        <div className="services-info">
+          {info.map((item, index) => {
+            return (
+              <div className="img">
+                <img src={item.image} alt="services" loading="lazy" />
+                <p>{item.title}</p>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
