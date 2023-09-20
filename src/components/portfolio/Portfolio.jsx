c1641432c2497b275aaea4fcaea0908a34c80eea
Roche Harold <rocheharold13@gmail.com>
2023-09-20 09:46:04 +0300

    update changes

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,7 +2,15 @@ import React from "react";
 import "./Portfolio.css";
 
 const Portfolio = () => {
-  return <div>Portfolio</div>;
+  return (
+    <div id="Portfolio" className="port-container">
+      <h3>Portfolio</h3>
+
+      <div className="port-img">
+        <img src="../../assets/dash1.png" alt="dash" />
+      </div>
+    </div>
+  );
 };
 
 export default Portfolio;
